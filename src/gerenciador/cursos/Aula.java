ccfebfc3a7802e065d4eccd8ca922f5394502a2f
Vitor Albano <vitoralbano@gmail.com>
2021-09-12 18:23:43 -0300

    sorting lists

---
+++ src/gerenciador/cursos/Aula.java
@@ -0,0 +1,29 @@
+package gerenciador.cursos;
+
+public class Aula implements Comparable<Aula>{
+    private String titulo;
+    private int tempo;
+
+    public Aula(String titulo, int tempo) {
+        this.titulo = titulo;
+        this.tempo = tempo;
+    }
+
+    public int getTempo() {
+        return tempo;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    @Override
+    public String toString() {
+        return "[Aula: " + this.titulo + ", " + this.tempo + " minutos]";
+    }
+
+    @Override
+    public int compareTo(Aula aula) {
+        return this.titulo.compareTo(aula.titulo);
+    }
+}
---
+++ src/gerenciador/cursos/TesteListaDeAula.java
@@ -0,0 +1,26 @@
+package gerenciador.cursos;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
+public class TesteListaDeAula {
+    public static void main(String[] args) {
+        Aula a1 = new Aula("Revisando as ArrayLists", 21);
+        Aula a2 = new Aula("Listas de objetos", 20);
+        Aula a3 = new Aula("Relacionamento de listas e objetos", 15);
+
+        ArrayList<Aula> aulas = new ArrayList<>();
+        aulas.add(a1);
+        aulas.add(a2);
+        aulas.add(a3);
+
+        System.out.println(aulas);
+
+        Collections.sort(aulas);
+        System.out.println(aulas);
+
+        Collections.sort(aulas, Comparator.comparing(Aula::getTempo));
+        System.out.println(aulas);
+    }
+}
