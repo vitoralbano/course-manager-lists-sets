70651842a6a0bad78f3219c482d6f25ba79da444
Vitor Albano <vitoralbano@gmail.com>
2021-09-19 13:20:20 -0300

    use linkedHashSet at Cursos to preserve insert order

--- src/gerenciador/cursos/Curso.java
+++ src/gerenciador/cursos/Curso.java
@@ -6,7 +6,7 @@ public class Curso {
     private String nome;
     private String instrutor;
     private List<Aula> aulas = new LinkedList<>();
-    private Set<Aluno> alunos = new HashSet<>();
+    private Set<Aluno> alunos = new LinkedHashSet<>();
 
     public Curso(String nome, String instrutor) {
         this.nome = nome;
